92a2e0da8ac8a34da8604fcc8e9f55ebc48a4928
fergus rooney <fergus592000@gmail.com>
2024-02-05 12:04:04 +0000

    Added fix for dumping memory in iverilog / gtkwave. Fixed some bugs in the fifo. Updated gitignore

--- fifo/verilog/fifo.v
+++ fifo/verilog/fifo.v
@@ -8,43 +8,49 @@ module fifo #(parameter data_word_size_g = 8,
             output wire r_empty_o, w_full_o
             );
             
-  reg [data_word_size_g-1 : 0] fifo_reg [$clog2(data_word_size_g)-1 : 0] = {$clog2(data_word_size_g){num_fifo_elements_g{1'b0}}};
-  reg [num_fifo_elements_g-1 : 0] fifo_counter = 0;
+    reg [data_word_size_g-1 : 0] fifo_reg [num_fifo_elements_g-1 : 0];
+    reg [num_fifo_elements_g-1 : 0] fifo_counter = {num_fifo_elements_g{1'b0}};
 
 
-  reg [data_word_size_g-1 : 0] r_data_reg = 0;
-  
+    reg [data_word_size_g-1 : 0] r_data_reg = 0;
 
-  reg r_ptr = 0;
-  reg w_ptr = 0;
+    reg [$clog2(num_fifo_elements_g)-1 : 0] r_ptr = 0;
+    reg [$clog2(num_fifo_elements_g)-1 : 0] w_ptr = 0;
 
-  always @(posedge clk_i)
-    if (rst_i == reset_pol_g) begin
-		fifo_counter <= {num_fifo_elements_g{1'b0}};
-        w_ptr  <= 0;
-        r_ptr <= 0;
-    end else if (clk_en_i == 1) begin
-        if (w_en_i | ~w_full_o) begin
-            fifo_reg[w_ptr] = w_data_i;
-            fifo_counter = fifo_counter + 1;
-            if (w_ptr == num_fifo_elements_g) 
-                w_ptr <= 0;
-            else 
-                w_ptr <= w_ptr + 1;
+    always @(posedge clk_i)
+        if (rst_i == reset_pol_g) begin
+            fifo_counter <= {num_fifo_elements_g{1'b0}};
+            w_ptr  <= 0;
+            r_ptr <= 0;
+        end else if (clk_en_i == 1) begin
+            if (w_en_i && ~w_full_o) begin
+                fifo_reg[w_ptr] = w_data_i;
+                fifo_counter = fifo_counter + 1;
+                if (w_ptr == num_fifo_elements_g) 
+                    w_ptr <= 0;
+                else 
+                    w_ptr <= w_ptr + 1;
+            end
+
+            if (r_en_i && ~r_empty_o) begin
+                r_data_reg <= fifo_reg[r_ptr];
+                fifo_counter <= fifo_counter - 1;
+                if  (r_ptr == num_fifo_elements_g)
+                    r_ptr <= 0;
+                else  
+                    r_ptr <= r_ptr + 1;
+            end
         end
-
-        if (r_en_i | ~r_empty_o) begin
-            r_data_reg <= fifo_reg[r_ptr];
-            fifo_counter <= fifo_counter - 1;
-            if  (r_ptr == num_fifo_elements_g)
-                r_ptr <= 0;
-            else  
-                r_ptr <= r_ptr + 1;
-        end
-    end
 
     assign r_data_o = r_data_reg;
 	assign w_full_o = fifo_counter == num_fifo_elements_g ? 1'b1 : 1'b0; 
     assign r_empty_o = fifo_counter == 0 ? 1'b1 : 1'b0;
 
+
+    // integer idx; // Dump out fifo contents for simulation
+    // initial begin
+    //   $dumpfile("dump1.vcd");
+    //   for (idx = 0; idx < num_fifo_elements_g; idx = idx + 1) $dumpvars(0, fifo_reg[idx]);
+    // end
+ 
 endmodule 
--- fifo/verilog/tb_fifo.v
+++ fifo/verilog/tb_fifo.v
@@ -9,14 +9,15 @@ module test;
     /* Make a regular pulsing clock. */
     reg clk = 1'b0, clk_en = 1'b0, reset = 1'b0;
     wire w_full, w_empty;
-    reg [data_word_size-1 : 0] w_data;
-    wire [data_word_size-1 : 0] r_data = 0;
+    reg [data_word_size-1 : 0] w_data = 0;
+    wire [data_word_size-1 : 0] r_data;
     reg w_en = 1'b0;
     reg r_en = 1'b0;
+    reg [data_word_size-1 : 0] fifo_reg [fifo_elements-1 : 0];
 
     always #(period/2) clk = !clk;
 
-    fifo #(data_word_size, fifo_elements, 1'b0)  fifo1(clk, reset, clk_en,r_en, w_en, w_data, r_data , r_empty, w_full);
+    fifo #(data_word_size, fifo_elements, 1'b0)  fifo1(clk, reset, clk_en,r_en, w_en, w_data, r_data , r_empty, w_full); 
 
     task test_read_write();
     begin
@@ -29,21 +30,23 @@ module test;
         w_data <= 69;
         @(posedge clk);
         w_data <= 42;
+        @(posedge clk);
         w_en <= 1'b0;
         # 30;
 
         @(posedge clk);
         r_en <= 1'b1;
         @(posedge clk);
+        @(posedge clk);
         if (r_data != 15) begin
-            $error ("Data 1 not equal to 15, equal to " + r_data);
+            $error ("Data 1 not equal to 15, equal to %d",  r_data);
         end
         @(posedge clk);
         if (r_data != 69)
-            $error ("Data 2 not equal to 69, equal to " + r_data);
+            $error ("Data 2 not equal to 69, equal to %d ", r_data);
         @(posedge clk);
         if (r_data != 42)
-            $error ("Data 2 not equal to 42, equal to " + r_data);
+            $error ("Data 2 not equal to 42, equal to %d", r_data);
         # 20;
         if (r_empty != 1'b1)
             $error ("Read_empty should be asserted when reading from empty fifo");
@@ -52,17 +55,24 @@ module test;
     end
     endtask
 
+    
     initial #5000 $finish;
 
+
+    integer idx;
+
+    
     initial begin
-        
         $dumpfile("fifo.vcd");
         $dumpvars(0, test);
+        
+        // Ugly way to dump out fifo contents for simulation for iverilog and gtkwave
+        for (idx = 0; idx < fifo_elements; idx = idx + 1) $dumpvars(0, fifo1.fifo_reg[idx]);
+
         reset <= 1'b0;
         #10;
         reset <= 1'b1;
         #10;
-
 
         test_read_write();
 
